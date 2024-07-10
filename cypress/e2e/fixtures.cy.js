abc10da87e1cd3e6d8856462fb6d0565a1a1808a
Am1d1v <dimadiman1adima@gmail.com>
2024-07-10 11:44:55 +0300

    Customer registration process

--- cypress/e2e/fixtures.cy.js
+++ cypress/e2e/fixtures.cy.js
@@ -1,9 +1,15 @@
 /// <reference types="cypress" />
 
-describe('template spec', () => {
+describe('Fixtures', () => {
 
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-  
+  it('should registrate customer', () => {
+    cy.visit('https://rahulshettyacademy.com/angularpractice/');
+
+    // Select "Name" input field and type and
+    cy.get(':nth-child(1) > .form-control').type('CustomerName_Female');
+
+    cy.get('#exampleFormControlSelect1').select(1);
+
+  });
+
 })
