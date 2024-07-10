09610c14a700e453b9d3c86363fa4fa6198ad12a
Am1d1v <dimadiman1adima@gmail.com>
2024-07-10 12:15:02 +0300

    Input registration data using fixtures

--- cypress/e2e/fixtures.cy.js
+++ cypress/e2e/fixtures.cy.js
@@ -2,18 +2,22 @@
 
 describe('Fixtures', () => {
 
+  let data = {};
+
   before(() => {
-    cy.fixture('example');
+    cy.fixture('Customer-Data').then(function(customerData){
+      data = customerData;
+    });
   });
 
   it('should registrate customer', () => {
     cy.visit('https://rahulshettyacademy.com/angularpractice/');
 
     // Select "Name" input field and type and
-    cy.get(':nth-child(1) > .form-control').type('CustomerName_Female');
+    cy.get(':nth-child(1) > .form-control').type(data.name);
 
     // Select "Female" in gender selection field
-    cy.get('#exampleFormControlSelect1').select("Female");
+    cy.get('#exampleFormControlSelect1').select(data.gender);
 
 
 
