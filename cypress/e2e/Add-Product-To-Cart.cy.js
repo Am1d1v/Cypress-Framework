cfd1000c89f1c90c9a46b32a25b70024c64e5a57
Am1d1v <dimadiman1adima@gmail.com>
2024-07-11 16:17:52 +0300

    Product selection

--- cypress/e2e/Add-Product-To-Cart.cy.js
+++ cypress/e2e/Add-Product-To-Cart.cy.js
@@ -4,6 +4,12 @@ describe('Add product to cart', () => {
   
     it('select product and add it into the cart', () => {
         cy.visit('https://rahulshettyacademy.com/angularpractice/shop');
+
+        cy.get('h4.card-title').each((product, index) => {
+            product.text().includes('iphone X');
+
+            cy.get('button.btn');
+        })
     });
   
   })
