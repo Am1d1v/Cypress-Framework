fc50a45b19da889e10d8545941af234aa62f810c
Am1d1v <dimadiman1adima@gmail.com>
2024-07-11 16:27:26 +0300

    Add product to the cart

--- cypress/e2e/Add-Product-To-Cart.cy.js
+++ cypress/e2e/Add-Product-To-Cart.cy.js
@@ -5,11 +5,20 @@ describe('Add product to cart', () => {
     it('select product and add it into the cart', () => {
         cy.visit('https://rahulshettyacademy.com/angularpractice/shop');
 
-        cy.get('h4.card-title').each((product, index) => {
-            product.text().includes('iphone X');
+        // The cart is empty
+        cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link').contains('0')
 
-            cy.get('button.btn');
+        cy.get('h4.card-title').each((product, productIndex) => {
+            // Add certain product to the cart
+            if(product.text().includes('iphone X')){
+                cy.get('button.btn').eq(productIndex).click();
+            }
+
         })
+
+        // Product was added to the cart
+        cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link').contains('1')
+
     });
   
   })
