d63ad30ed1c75640e0e590ccb354ec00a1572997
Am1d1v <dimadiman1adima@gmail.com>
2024-07-11 16:39:02 +0300

    Add certain product to the cart using cypress commands

--- cypress/e2e/Add-Product-To-Cart.cy.js
+++ cypress/e2e/Add-Product-To-Cart.cy.js
@@ -2,7 +2,21 @@
 
 describe('Add product to cart', () => {
   
-    it('select product and add it into the cart', () => {
+    it('select product "iphone X" and add it into the cart', () => {
+        cy.visit('https://rahulshettyacademy.com/angularpractice/shop');
+
+        // The cart is empty
+        cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link').contains('0')
+
+        // Add certain product to the cart
+        cy.addProduct('iphone X');
+
+        // Product was added to the cart
+        cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link').contains('1')
+
+    });
+
+    it('select product "Blackberry" and add it into the cart', () => {
         cy.visit('https://rahulshettyacademy.com/angularpractice/shop');
 
         // The cart is empty
