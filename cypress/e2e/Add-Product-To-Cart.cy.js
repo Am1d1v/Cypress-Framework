a4a97e614a86d8538b598921613e4d21ff187a77
Am1d1v <dimadiman1adima@gmail.com>
2024-07-13 10:39:20 +0300

    Select and add multiple products into the cart

--- cypress/e2e/Add-Product-To-Cart.cy.js
+++ cypress/e2e/Add-Product-To-Cart.cy.js
@@ -18,17 +18,33 @@ describe('Add product to cart', () => {
 
     });
 
-    it.only('select product "Blackberry" and add it into the cart', () => {
+    it('select product "Blackberry" and add it into the cart', () => {
         cy.visit('https://rahulshettyacademy.com/angularpractice/shop');
 
         // The cart is empty
         cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link').contains('0')
 
         // Add certain product to the cart
-        cy.addProduct('iphone X');
+        cy.addProduct('Blackberry');
 
         // Product was added to the cart
         cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link').contains('1')
+        cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link').click();
+        cy.get('tbody').should('have.length.above', 0);
+    });
+
+    it.only('select products "Blackberry" & "iphone X" then add it into the cart', () => {
+        cy.visit('https://rahulshettyacademy.com/angularpractice/shop');
+
+        // The cart is empty
+        cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link').contains('0')
+
+        // Add multiple products to the cart
+        cy.addProduct('Blackberry');
+        cy.addProduct('iphone X');
+
+        // Product was added to the cart
+        cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link').contains('2')
         cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link').click();
         cy.get('tbody').should('have.length.above', 0);
     });
