57e84355fd315c6122907374d4851069ccc809bc
Am1d1v <dimadiman1adima@gmail.com>
2024-07-20 20:58:07 +0300

    Mochawesome tests reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  defaultCommandTimeout: 6000,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
+
 });
